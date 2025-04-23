037a0e82764482f601ca11fd80060d2a5e497620
seekCat969 <zhangmingzhuo969@gmail.com>
2025-04-23 13:17:34 +0800

    2025.4.23/13

---
+++ src/main/java/com/seekcat/reggie/RegierApplication.java
@@ -0,0 +1,13 @@
+package com.seekcat.reggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seekcat/reggie/RegierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seekcat.reggie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
